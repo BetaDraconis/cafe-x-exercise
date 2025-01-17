800280746effa1528dc8db21b92d722377f4f1d2
Luke.Jones Work Account <47660116+BetaDraconis@users.noreply.github.com>
2025-01-17 18:59:40 +0000

    amend generateTotalBill method to pass initial test

--- src/main/scala/models/Menu.scala
+++ src/main/scala/models/Menu.scala
@@ -3,5 +3,5 @@ package models
 case class Menu(items: Map[String, Double])
 
 object Menu {
-  def generateTotalBill(menu: Menu, billItems: Seq[String]): Double = ???
+  def generateTotalBill(menu: Menu, billItems: Seq[String]): Double = 0d
 }
