442241c6d50b7f3e1c07cfc31e79721e12831493
Luke.Jones Work Account <47660116+BetaDraconis@users.noreply.github.com>
2025-01-17 19:02:43 +0000

    amend implementation of generateTotalBill method to succeed new test scenarios

--- src/main/scala/models/Menu.scala
+++ src/main/scala/models/Menu.scala
@@ -3,5 +3,6 @@ package models
 case class Menu(items: Map[String, Double])
 
 object Menu {
-  def generateTotalBill(menu: Menu, billItems: Seq[String]): Double = 0d
+  def generateTotalBill(menu: Menu, billItems: Seq[String]): Double =
+    billItems.foldLeft(0d)((sum, nextElem) => sum + menu.items(nextElem))
 }
